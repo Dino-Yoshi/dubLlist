464cbf7ff9a1665acbb833f83234c1b7be5f9328
Dino-Yoshi <dinoyoshi.chau@gmail.com>
2026-09-09 17:04:58 -0700

    declared doubly llist, 11 methods

---
+++ DLList.h
@@ -0,0 +1,50 @@
+//
+// Created by dinoyoshi on 9/9/26.
+//
+
+#ifndef DUBLLIST_DLLIST_H
+#define DUBLLIST_DLLIST_H
+
+/* Necessary features
+* PushFront(Key)
+• PushBack(Key)
+• TopFront()
+• PopFront()
+• TopBack()
+• PopBack()
+• Find(Key)
+• Erase(Key)
+• Empty()
+• AddBefore(Node, Key)
+• AddAfter(Node, Key)
+ */
+
+template <typename T>
+class DLList {
+private:
+    int numNodes;
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
+    };
+
+    Node* tail;
+public:
+    Node* head;
+    DLList();
+    bool PushFront(T);
+    bool PushBack(T);
+    T TopFront();
+    T PopFront();
+    T TopBack();
+    T PopBack();
+    bool Find(T);
+    bool Erase(T);
+    bool Empty();
+    bool AddBefore(Node*, T);
+    bool AddAfter(Node*, T);
+};
+
+
+#endif //DUBLLIST_DLLIST_H
